16977249a96635898ba6891268ddf131ef236447
patsul12 <patsul12@gmail.com>
2016-01-15 08:21:31 -0800

    allow user to create a new word

--- app.rb
+++ app.rb
@@ -1,8 +1,20 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require './lib/definition'
-require './lib/definition'
+require './lib/word'
 
 get '/' do
+  @words = Word.all
   erb :index
+end
+
+get '/words/new' do
+  erb :new_word_form
+end
+
+post '/words/new' do
+  name = params[:name]
+  word = Word.new({:name => name})
+  word.save
+  redirect '/'
 end
